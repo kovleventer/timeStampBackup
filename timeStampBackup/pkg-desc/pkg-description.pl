420608f5e4c4947718858559222c16297db2c8f3
Kovleventer <kovleventer@gmail.com>
2022-03-31 19:23:11 +0200

    Version 1.2.1
    Polish translation added by Rafkus_pl
    Eliminated code duplication regarding LibreOffice and OpenOffice
    selection

---
+++ timeStampBackup/pkg-desc/pkg-description.pl
@@ -0,0 +1,11 @@
+Rozszerzenie to działa w ApacheOpenOffice i LibreOffice. Umożliwia użytkownikowi zapisanie bieżącego dokumentu w dwóch lokacjach jednocześnie:
+	* Oryginału w miejscu przeznaczenia oraz 
+	* Kopii o nazwie oryginalnego pliku uzupełnionej o znacznik czasowy.
+
+Położenie kopii wynika z ustawień parametru kopii zapasowych w "Narzędzia → Opcje → OpenOffice/LibreOffice → Ścieżki → Kopie zapasowe".
+Zaleca się dostosowanie ścieżki do folderu kopii zapasowych do przyjaznego miejsca.
+
+Rozszerzenie dodaje dwa nowe polecenia w menu Plik oraz ikonę zapisu na standardowym pasku narzędzi.
+
+Licencja: GPL v3
+Twórca oryginalny: Kovács Tibor
